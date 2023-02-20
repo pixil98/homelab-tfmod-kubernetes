511e6d4d9bcb6af7089c7dec580415db38ae6cdf
Aaron Reisman <aaron@reisman.org>
2023-02-19 21:23:42 -0600

    base64 encode secret contents

--- modules/flux/outputs.tf
+++ modules/flux/outputs.tf
@@ -4,9 +4,9 @@ locals {
   ${templatefile("${path.module}/secret.tftpl", {
     name                = data.flux_sync.main.secret
     namespace           = data.flux_sync.main.namespace
-    identity_base64     = tls_private_key.main.private_key_pem
-    identity_pub_base64 = tls_private_key.main.public_key_pem
-    knownhosts_base64   = local.known_hosts
+    identity_base64     = base64encode(tls_private_key.main.private_key_pem)
+    identity_pub_base64 = base64encode(tls_private_key.main.public_key_pem)
+    knownhosts_base64   = base64encode(local.known_hosts)
   })}
   ${data.flux_install.main.content}
   ${data.flux_sync.main.content}
