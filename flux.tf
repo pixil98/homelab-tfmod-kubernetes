78215e913c506ac0feabecaae1366a20d02fb0a3
Aaron Reisman <aaron@reisman.org>
2024-01-04 00:04:26 -0600

    Specify strings correctly

--- flux.tf
+++ flux.tf
@@ -5,7 +5,7 @@ resource "tls_private_key" "flux" {
 
 resource "github_repository_deploy_key" "flux" {
   title      = "flux - ${var.flux_github_branch}"
-  repository = ${var.flux_github_repo_name}
+  repository = var.flux_github_repo_name
   key        = tls_private_key.flux.public_key_openssh
   read_only  = true
 }
@@ -13,5 +13,5 @@ resource "github_repository_deploy_key" "flux" {
 resource "flux_bootstrap_git" "this" {
   depends_on = [github_repository_deploy_key.flux]
 
-  path = "${var.flux_github_target_path}"
+  path = var.flux_github_target_path
 }
