d033bb123c4394ff5d0364e5c3152234c398ca74
pixil98 <46978190+pixil98@users.noreply.github.com>
2024-01-04 20:51:23 -0600

    Switch to new flux bootstrap method (#4)
    * Initial attempt at using new flux bootstrap
    
    * Specify git repo branch
    
    * remove owner from flux repo name
    
    * Specify strings correctly
    
    * specify proper api server url for flux to use
    
    * Remove unused module
    
    * Allow flux to be disabled
    
    * Add tls provider
    
    * Update providers
    
    * Configure proxmox provider here?

--- flux.tf
+++ flux.tf
@@ -1,9 +1,20 @@
-module "flux" {
-  source = "./modules/flux"
+resource "tls_private_key" "flux" {
   count  = var.flux_enabled ? 1 : 0
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P256"
+}
 
-  flux_github_target_path = var.flux_github_target_path
-  flux_github_repo_owner  = var.flux_github_repo_owner
-  flux_github_repo_name   = var.flux_github_repo_name
-  flux_github_branch      = var.flux_github_branch
+resource "github_repository_deploy_key" "flux" {
+  count  = var.flux_enabled ? 1 : 0
+  title      = "flux - ${var.flux_github_branch}"
+  repository = var.flux_github_repo_name
+  key        = tls_private_key.flux[0].public_key_openssh
+  read_only  = true
+}
+
+resource "flux_bootstrap_git" "flux" {
+  count  = var.flux_enabled ? 1 : 0
+  depends_on = [github_repository_deploy_key.flux[0]]
+
+  path = var.flux_github_target_path
 }
--- main.tf
+++ main.tf
@@ -1,21 +1,46 @@
 terraform {
   required_version = ">= 1.3.6"
   required_providers {
-    proxmox = {
-      source = "bpg/proxmox"
-      version = "0.42.0"
+    flux = {
+      source = "fluxcd/flux"
+      version = "1.2.2"
     }
-    rke = {
-      source  = "rancher/rke"
-      version = "1.4.3"
+    github = {
+      source  = "integrations/github"
+      version = "5.43.0"
     }
     local = {
       source  = "hashicorp/local"
       version = "2.4.1"
     }
-    github = {
-      source  = "integrations/github"
-      version = "5.42.0"
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.43.0"
+    }
+    rke = {
+      source  = "rancher/rke"
+      version = "1.4.3"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "4.0.5"
+    }
+  }
+}
+
+provider "flux" {
+  kubernetes = {
+    host                   = rke_cluster.cluster.api_server_url
+    client_certificate     = rke_cluster.cluster.client_cert
+    client_key             = rke_cluster.cluster.client_key
+    cluster_ca_certificate = rke_cluster.cluster.ca_crt
+  }
+  git = {
+    url    = "ssh://git@github.com/${var.flux_github_repo_owner}/${var.flux_github_repo_name}.git"
+    branch = var.flux_github_branch
+    ssh    = {
+      username    = "git"
+      private_key = var.vm_user_privatekey
     }
   }
 }
@@ -23,4 +48,10 @@ terraform {
 provider "github" {
   owner = var.flux_github_repo_owner
   token = var.flux_github_token
+}
+
+provider "proxmox" {
+  endpoint = var.proxmox_endpoint
+  username = var.proxmox_user
+  password = var.proxmox_password
 }
