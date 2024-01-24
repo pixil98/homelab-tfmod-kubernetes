b6c2ad0efbf3477df3d5dacfd91b68e1c4af93ac
Aaron Reisman <aaron@reisman.org>
2024-01-24 00:11:38 -0600

    have k8s manifests depend o nthe cluster being created first

--- flux.tf
+++ flux.tf
@@ -1,11 +1,11 @@
 resource "tls_private_key" "flux" {
-  count  = var.flux_enabled ? 1 : 0
+  count       = var.flux_enabled ? 1 : 0
   algorithm   = "ECDSA"
   ecdsa_curve = "P256"
 }
 
 resource "github_repository_deploy_key" "flux" {
-  count  = var.flux_enabled ? 1 : 0
+  count      = var.flux_enabled ? 1 : 0
   title      = "flux - ${var.flux_github_branch}"
   repository = var.flux_github_repo_name
   key        = tls_private_key.flux[0].public_key_openssh
@@ -13,21 +13,22 @@ resource "github_repository_deploy_key" "flux" {
 }
 
 resource "flux_bootstrap_git" "flux" {
-  count  = var.flux_enabled ? 1 : 0
+  count      = var.flux_enabled ? 1 : 0
   depends_on = [github_repository_deploy_key.flux[0]]
 
-  path = var.flux_github_target_path
+  path       = var.flux_github_target_path
 }
 
 # Flux values configmap
 data "jq_query" "flux_values" {
-  count  = var.flux_enabled ? 1 : 0
-  data = var.flux_values_json
+  count = var.flux_enabled ? 1 : 0
+  data  = var.flux_values_json
   query = "[paths(scalars|true) as $p | {([$p[]] | join(\"_\")): getpath($p)}] | reduce .[] as $item ({}; . * $item)"
 }
 
 resource "kubernetes_config_map" "flux_values" {
-  count  = var.flux_enabled ? 1 : 0
+  count = var.flux_enabled ? 1 : 0
+  depends_on = [rke_cluster.cluster]
   metadata {
     name      = "flux-values"
     namespace = flux_bootstrap_git.flux[0].namespace
@@ -37,15 +38,16 @@ resource "kubernetes_config_map" "flux_values" {
 }
 
 resource "kubernetes_manifest" "flux_core_gitrepo" {
-  count  = (var.flux_enabled && var.flux_core_repository != null) ? 1 : 0
+  count    = (var.flux_enabled && var.flux_core_repository != null) ? 1 : 0
+  depends_on = [rke_cluster.cluster]
   manifest = {
     "apiVersion" = "source.toolkit.fluxcd.io/v1"
     "kind"       = "GitRepository"
-    "metadata" = {
+    "metadata"   = {
       "name"      = "flux-core"
       "namespace" = flux_bootstrap_git.flux[0].namespace
     }
-    "spec" = {
+    "spec"       = {
       interval = "5m"
       url      = var.flux_core_repository
       ref      = {
@@ -56,15 +58,16 @@ resource "kubernetes_manifest" "flux_core_gitrepo" {
 }
 
 resource "kubernetes_manifest" "flux_core_kustomization" {
-  count  = (var.flux_enabled && var.flux_core_repository != null) ? 1 : 0
+  count    = (var.flux_enabled && var.flux_core_repository != null) ? 1 : 0
+  depends_on = [rke_cluster.cluster]
   manifest = {
     "apiVersion" = "kustomize.toolkit.fluxcd.io/v1"
     "kind"       = "Kustomization"
-    "metadata" = {
+    "metadata"   = {
       "name"      = "flux-core"
       "namespace" = flux_bootstrap_git.flux[0].namespace
     }
-    "spec" = {
+    "spec"       = {
       interval  = "10m"
       sourceRef = {
         kind = "GitRepository"
