d3eb015c895f769ff15b31aa6de267f9978240fe
Aaron Reisman <aaron@reisman.org>
2024-06-01 22:56:43 -0500

    Remove secret generation, it was cool but lead to problems

--- flux.tf
+++ flux.tf
@@ -1,7 +1,3 @@
-locals {
-  GENERATED_SECRET_STRING = "<generated>"
-}
-
 resource "tls_private_key" "flux" {
   count       = var.flux_enabled ? 1 : 0
   algorithm   = "ECDSA"
@@ -24,43 +20,46 @@ resource "flux_bootstrap_git" "flux" {
 }
 
 # Flux values configmap
-data "jq_query" "flux_values" {
+data "jq_query" "flux_core_values" {
   count = var.flux_enabled ? 1 : 0
   data  = var.flux_values_json
   query = "[paths(scalars|true) as $p | {([$p[]] | join(\"_\")): getpath($p)}] | reduce .[] as $item ({}; . * $item) | with_entries(.key |= \"vals_\" + .)"
 }
 
-resource "kubernetes_config_map" "flux_values" {
+resource "kubernetes_config_map" "flux_core_values" {
   count = var.flux_enabled ? 1 : 0
   metadata {
     name      = "flux-values"
     namespace = flux_bootstrap_git.flux[0].namespace
   }
 
-  data = jsondecode(data.jq_query.flux_values[0].result)
+  data = jsondecode(data.jq_query.flux_core_values[0].result)
 }
 
 # Flux secrets configmap
-data "jq_query" "flux_secrets" {
+data "jq_query" "flux_core_secrets" {
   count = var.flux_enabled ? 1 : 0
   data  = var.flux_secrets_json
   query = "[paths(scalars|true) as $p | {([$p[]] | join(\"_\")): getpath($p)}] | reduce .[] as $item ({}; . * $item) | with_entries(.key |= \"secrets_\" + .)"
 }
 
-resource "random_password" "generated_secrets" {
-  for_each = { for k, v in jsondecode(data.jq_query.flux_secrets[0].result): k => v if v == local.GENERATED_SECRET_STRING }
-  length  = 50
-  special = false
-}
-
-resource "kubernetes_secret" "flux_secrets" {
+// locals {
+//   GENERATED_SECRET_STRING = "<generated>"
+// }
+// resource "random_password" "generated_secrets" {
+//   for_each = { for k, v in jsondecode(data.jq_query.flux_secrets[0].result): k => v if v == local.GENERATED_SECRET_STRING }
+//   length  = 50
+//   special = false
+// }
+resource "kubernetes_secret" "flux_core_secrets" {
   count = var.flux_enabled ? 1 : 0
   metadata {
     name      = "flux-secrets"
     namespace = flux_bootstrap_git.flux[0].namespace
   }
   
-  data = { for k, v in jsondecode(data.jq_query.flux_secrets[0].result): k => (v == local.GENERATED_SECRET_STRING ? random_password.generated_secrets[k].result : v) }
+  // data = { for k, v in jsondecode(data.jq_query.flux_secrets[0].result): k => (v == local.GENERATED_SECRET_STRING ? random_password.generated_secrets[k].result : v) }
+  data = jsondecode(data.jq_query.flux_core_secrets[0].result)
 }
 
 resource "kubectl_manifest" "flux_core_gitrepository" {
