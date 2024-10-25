6ef00716d04cfab0bf20e120d5b76e3a031c6e49
Aaron Reisman <areisman@epic.com>
2024-10-25 16:51:44 -0500

    manually pretty it up; run init

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.3.6"
   required_providers {
     flux = {
-      source = "fluxcd/flux"
+      source  = "fluxcd/flux"
       version = "1.4.0"
     }
     github = {
@@ -10,15 +10,15 @@ terraform {
       version = "6.3.1"
     }
     jq = {
-      source = "massdriver-cloud/jq"
+      source  = "massdriver-cloud/jq"
       version = "0.2.1"
     }
     kubectl = {
-      source = "gavinbunney/kubectl"
+      source  = "gavinbunney/kubectl"
       version = "1.14.0"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = "2.33.0"
     }
     local = {
@@ -26,11 +26,11 @@ terraform {
       version = "2.5.2"
     }
     proxmox = {
-      source = "bpg/proxmox"
+      source  = "bpg/proxmox"
       version = "0.66.3"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.6.3"
     }
     rke = {
@@ -38,7 +38,7 @@ terraform {
       version = "1.5.0"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
       version = "4.0.6"
     }
   }
@@ -54,7 +54,7 @@ provider "flux" {
   git = {
     url    = "ssh://git@github.com/${var.flux_github_repo_owner}/${var.flux_github_repo_name}.git"
     branch = var.flux_github_branch
-    ssh    = {
+    ssh = {
       username    = "git"
       private_key = var.vm_user_privatekey
     }
